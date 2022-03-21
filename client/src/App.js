aba74f243e5f7faaa7b6ec54efce9d9809ba4722
Nihal Vishal Gupta <nihalng786@gmail.com>
2022-03-21 18:59:35 +0530

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Auth from './components/Auth/Auth';
+
+function App() {
+  return (
+    <div>
+       <Auth />
+    </div>
+  );
+}
+
+export default App;
